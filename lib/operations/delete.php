23a99b7a45981fa8f34a6013c737b3257be57913
Olivier Laviale <contact@weirdog.com>
2013-07-22 16:05:02 +0200

    Retrieved module operations from ICanBoogie

---
+++ lib/operations/delete.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie;
+
+use ICanBoogie\I18n\FormattedString;
+
+/**
+ * Deletes a record.
+ */
+class DeleteOperation extends Operation
+{
+	/**
+	 * Modifies the following controls:
+	 *
+	 * - PERMISSION: MANAGE
+	 * - RECORD: true
+	 * - OWNERSHIP: true
+	 */
+	protected function get_controls()
+	{
+		return array
+		(
+			self::CONTROL_PERMISSION => Module::PERMISSION_MANAGE,
+			self::CONTROL_RECORD => true,
+			self::CONTROL_OWNERSHIP => true
+		)
+
+		+ parent::get_controls();
+	}
+
+	protected function validate(Errors $errors)
+	{
+		return true;
+	}
+
+	/**
+	 * Delete the target record.
+	 */
+	protected function process()
+	{
+		$key = $this->key;
+
+		if (!$this->module->model->delete($key))
+		{
+			throw new Exception
+			(
+				'Unable to delete the record %key from %module.', array
+				(
+					'key' => $key,
+					'module' => $this->module->title
+				)
+			);
+		}
+
+		if ($this->request['redirect_to'])
+		{
+			$this->response->location = $this->request['redirect_to'];
+		}
+
+		$this->response->message = new FormattedString('The record %key has been delete from %module.', array('key' => $key, 'module' => $this->module->title));
+
+		return $key;
+	}
+}
