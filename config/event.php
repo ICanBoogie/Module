75641dce1a970da0a14ce53e811350f48cf9361c
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-18 23:54:00 +0200

    Use callable notation for events

--- config/event.php
+++ config/event.php
@@ -7,16 +7,14 @@ use ICanBoogie\Application;
 use ICanBoogie\Application\ClearCacheEvent;
 use ICanBoogie\Binding\Routing\BeforeSynthesizeRoutesEvent;
 
-$hooks = Hooks::class . '::';
-
 return [
 
-	ClearCacheEvent::qualify(Application::class) => $hooks . 'on_app_clear_cache',
+	ClearCacheEvent::qualify(Application::class) => [ Hooks::class, 'on_app_clear_cache' ],
+	BeforeSynthesizeRoutesEvent::TYPE => [ Hooks::class, 'before_synthesize_routes' ]
+
 	/* @deprecated */
 //	ICanBoogie\View\View::class . '::alter' => $hooks . 'on_view_alter',
 	/* @deprecated */
 //	ICanBoogie\HTTP\RequestDispatcher::class . '::alter' => $hooks . 'on_alter_request_dispatcher',
-
-	BeforeSynthesizeRoutesEvent::TYPE => $hooks . 'before_synthesize_routes'
 
 ];
