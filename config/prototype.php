5e60aaecc44731d34c1d3200c1639a5537f3c154
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-23 14:45:50 +0200

    Use prototype's ConfigBuilder

--- config/prototype.php
+++ config/prototype.php
@@ -5,16 +5,14 @@ namespace ICanBoogie\Module;
 use ICanBoogie;
 use ICanBoogie\Application;
 use ICanBoogie\Binding\Module\Hooks;
+use ICanBoogie\Binding\Prototype\ConfigBuilder;
 use ICanBoogie\Routing\ControllerAbstract;
 
-return [
-
-	Application::class . '::lazy_get_modules' => [ Hooks::class, 'get_modules' ],
-	Application::class . '::lazy_get_models' => [ Hooks::class, 'get_models' ],
-	ControllerAbstract::class . '::lazy_get_module' => [ Hooks::class, 'controller_get_module' ],
-	ControllerAbstract::class . '::get_model' => [ Hooks::class, 'controller_get_model' ],
-	ControllerAbstract::class . '::lazy_get_records_fetcher' => [ Hooks::class, 'controller_lazy_get_records_fetcher' ],
-	ControllerAbstract::class . '::fetch_records' => [ Hooks::class, 'controller_fetch_records' ],
-	ControllerAbstract::class . '::fetch_record' => [ Hooks::class, 'controller_fetch_record' ],
-
-];
+return fn(ConfigBuilder $config) => $config
+	->bind(Application::class, 'lazy_get_modules', [ Hooks::class, 'get_modules' ])
+	->bind(Application::class, 'lazy_get_models', [ Hooks::class, 'get_models' ])
+	->bind(ControllerAbstract::class, 'lazy_get_module', [ Hooks::class, 'controller_get_module' ])
+	->bind(ControllerAbstract::class, 'get_model', [ Hooks::class, 'controller_get_model' ])
+	->bind(ControllerAbstract::class, 'lazy_get_records_fetcher', [ Hooks::class, 'controller_lazy_get_records_fetcher' ])
+	->bind(ControllerAbstract::class, 'fetch_records', [ Hooks::class, 'controller_fetch_records' ])
+	->bind(ControllerAbstract::class, 'fetch_record', [ Hooks::class, 'controller_fetch_record' ]);
