295162cbe894d5dde961249edd0886a9edb0db8b
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-23 15:58:07 +0200

    Use event's ConfigBuilder

--- config/event.php
+++ config/event.php
@@ -5,15 +5,13 @@ namespace ICanBoogie\Module;
 use ICanBoogie;
 use ICanBoogie\Application;
 use ICanBoogie\Application\ClearCacheEvent;
+use ICanBoogie\Binding\Event\ConfigBuilder;
 
-return [
-
-	ClearCacheEvent::for(Application::class) => [ Hooks::class, 'on_app_clear_cache' ],
+return fn(ConfigBuilder $config) => $config
+	->attach_to(Application::class, ClearCacheEvent::class, [ Hooks::class, 'on_app_clear_cache' ]);
 	/* @deprecated */
 //	BeforeSynthesizeRoutesEvent::class => [ Hooks::class, 'before_synthesize_routes' ]
 	/* @deprecated */
 //	ICanBoogie\View\View::class . '::alter' => $hooks . 'on_view_alter',
 	/* @deprecated */
 //	ICanBoogie\HTTP\RequestDispatcher::class . '::alter' => $hooks . 'on_alter_request_dispatcher',
-
-];
